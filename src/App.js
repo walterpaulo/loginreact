83f4dad473d19fee75cd524c1d01e412e007c1c1
walterpaulo <walter0paulo@hotmail.com>
2021-12-16 02:26:36 -0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 import Header from './components/Herder'
 import Footer from './components/Footer'
+import { Routes } from './Routes';
 
 function App(){
   return (
-          <div>
-            <Header/>
-            <div>
-             Páginas
-            </div>
-            
+      <BrowserRouter>
+          <Header/>
+          
+          <Routes/>
 
-            <Footer/>
-          </div>
+          <Footer/>
+      
+      </BrowserRouter>
   );
 }
 
